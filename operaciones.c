58c537ee3d0db640568e756671f5198c08151959
adelgadoangio <adelgadoangio@frba.utn.edu.ar>
2026-04-29 20:05:09 -0300

    wip: experimento roto

--- operaciones.c
+++ operaciones.c
@@ -21,7 +21,6 @@ int multiplicar(int a, int b) {
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 
-int esPar(int n) {
-    return (n % 2) == 0; /* version main */
+int dividir(int a, int b) {
+    return a - b; /* bug intencional */
 }
-
